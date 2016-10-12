3ec531d4d8a3fb2c1b7097bec0aa54af9a70807e
recepinanc <recepinanc96@gmail.com>
2016-10-12 22:36:39 +0300

    Setup the logic and add some tests

--- lib/tobinary.rb
+++ lib/tobinary.rb
@@ -5,12 +5,34 @@ module Tobinary
     number = self
 
     if number < 0 || number > 65536
-      # todo
       puts 'Sorry cannot convert this number to binary'
       number
     else
-      puts ''
+      if number < 4
+        digits = Array.new(2,0)
+      elsif number < 256
+        digits = Array.new(8,0)
+      elsif number < 65536
+        digits = Array.new(16,0)
+      end
+
+      index = digits.length-1
+      if number == 1
+        digits[index] = 1
+      else
+        while number > 2
+          reminder = number %2
+          digits[index] = reminder
+          index -= 1
+          number /= 2
+          if number == 1
+            digits[index] = 1
+          end
+        end
+      end
     end
+
+    digits.join
   end
 end
 
--- spec/tobinary_spec.rb
+++ spec/tobinary_spec.rb
@@ -3,17 +3,26 @@ require "spec_helper"
   describe Tobinary do
     context 'Numbers lower than 4' do
       it 'should return: 01' do
+        expect(1.to_binary).to eq '01'
+      end 
+      it 'should return: 11' do
         expect(3.to_binary).to eq '11'
       end 
     end
 
     context 'Numbers lower than 256' do
+      it 'should return: 01111011' do
+        expect(123.to_binary).to eq '01111011'
+      end 
       it 'should return: 11111111' do
         expect(255.to_binary).to eq '11111111'
       end 
     end
 
     context 'Numbers lower than 65536' do
+      it 'should return: 0111111111111111' do
+        expect(32767.to_binary).to eq '0111111111111111'
+      end 
       it 'should return: 1111111111111111' do
         expect(65535.to_binary).to eq '1111111111111111'
       end 
