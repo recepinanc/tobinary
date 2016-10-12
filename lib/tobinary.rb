4c91edc88737e4766df168135e2b7798a914183a
recepinanc <recepinanc96@gmail.com>
2016-10-12 22:07:51 +0300

    Fix test, add some checks and start coding the method

--- lib/tobinary.rb
+++ lib/tobinary.rb
@@ -4,8 +4,10 @@ module Tobinary
   def to_binary
     number = self
 
-    if number > 0
+    if number < 0 || number > 65536
       # todo
+      puts 'Sorry cannot convert this number to binary'
+      number
     else
       puts ''
     end
--- spec/tobinary_spec.rb
+++ spec/tobinary_spec.rb
@@ -1,11 +1,23 @@
 require "spec_helper"
 
-describe Tobinary do
-  it "has a version number" do
-    expect(Tobinary::VERSION).not_to be nil
-  end
+  describe Tobinary do
+    context 'Numbers lower than 4' do
+      it 'should return: 01' do
+        expect(3.to_binary).to eq '11'
+      end 
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    context 'Numbers lower than 256' do
+      it 'should return: 11111111' do
+        expect(255.to_binary).to eq '11111111'
+      end 
+    end
+
+    context 'Numbers lower than 65536' do
+      it 'should return: 1111111111111111' do
+        expect(65535.to_binary).to eq '1111111111111111'
+      end 
+    end
+
+    #others will be added
   end
-end
